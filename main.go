8cc8300b46feb6421b2e12f7428ba4195f7783e0
Mann Shah <shahmann0143@gmail.com>
2024-08-28 10:38:14 +0530

    Add test step to GitHub Actions workflow

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"io/ioutil"
+	"net/http/httptest"
+	"testing"
 )
 
 func Add(a, b int) int {
@@ -31,6 +34,21 @@ func additionHandler(w http.ResponseWriter, r *http.Request) {
 	result := Add(a, b)
 
 	fmt.Fprintf(w, "%d", result)
+}
+
+func TestAdditionHandler(t *testing.T) {
+	// Test case 1
+	req1 := httptest.NewRequest("GET", "/addition?a=3&b=5", nil)
+	w1 := httptest.NewRecorder()
+	additionHandler(w1, req1)
+	resp1 := w1.Result()
+	defer resp1.Body.Close()
+	body1, _ := ioutil.ReadAll(resp1.Body)
+	result1, _ := strconv.Atoi(string(body1))
+	expected1 := 8
+	if result1 != expected1 {
+		t.Errorf("Test case 1 failed, expected %d but got %d", expected1, result1)
+	}
 }
 
 func main() {
